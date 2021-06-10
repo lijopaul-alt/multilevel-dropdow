ab922623919355cb7b3c048acd20b679fed06a33
lijopaul-alt <lijopaul19@gmail.com>
2021-06-10 20:34:42 +0530

    final-update

--- src/App.js
+++ src/App.js
@@ -4,69 +4,67 @@ import "./App.css";
 import Dropdown from "react-multilevel-dropdown";
 
 const App = () => {
-  const [Catergories] = useState([
-    { id: 1, value: "continental" },
-    { id: 2, value: "chinese" },
-    { id: 3, value: "south indian" },
-    { id: 4, value: "mexican" },
-    { id: 5, value: "american" },
-  ]);
-  const [dishes] = useState([
-    { id: 6, value: "Dosa" },
-    { id: 7, value: "uthappam" },
-    { id: 8, value: "parotta" },
-    { id: 9, value: "chicken" },
-  ]);
+  // const categories=useState({id:1,value:'continental'
 
-  const [variety] = useState([
-    { id: 11, value: "chciken masala" },
-    { id: 12, value: "chicken gravy" },
-    { id: 13, value: "chicken 65" },
-    { id: 14, value: "chiken varuval" },
-  ]);
+  //},{id:2,value:'punjabi'},{id:3,value:'chettinad'},{id:'4,'south indian'})
+  // const [dishes] = useState([
+  //   { id: 6, value: "Dosa" },
+  //   { id: 7, value: "uthappam" },
+  //   { id: 8, value: "parotta" },
+  //   { id: 9, value: "chicken" },
+  // ]);
 
-  const nestedObjects = Catergories.map((Catergories) => {
-    if (Catergories.value === "south indian") {
-      return (
-        <Dropdown.Item key={Catergories.id} className="item">
-          <p>
-            {Catergories.value} <i className="fa fa-sort-desc icon"></i>
-          </p>
-          <Dropdown.Submenu position="right">
-            {dishes.map((dishes) => {
-              if (dishes.value === "chicken") {
-                return (
-                  <Dropdown.Item key={dishes.id} className="item">
-                    {dishes.value}
-                    <i className="fa fa-sort-desc icon"></i>
-                    <Dropdown.Submenu position="right">
-                      {variety.map((variety) => (
-                        <Dropdown.Item key={variety.key} className="item">
-                          {variety.value}
-                        </Dropdown.Item>
-                      ))}
-                    </Dropdown.Submenu>
-                  </Dropdown.Item>
-                );
-              } else {
-                return (
-                  <Dropdown.Item key={dishes.id} className="item">
-                    {dishes.value}
-                  </Dropdown.Item>
-                );
-              }
-            })}
-          </Dropdown.Submenu>
-        </Dropdown.Item>
-      );
-    } else {
-      return (
-        <Dropdown.Item key={Catergories.id} className="item">
-          <p>{Catergories.value}</p>
-        </Dropdown.Item>
-      );
-    }
-  });
+  // const [variety] = useState([
+  //   { id: 11, value: "chciken masala" },
+  //   { id: 12, value: "chicken gravy" },
+  //   { id: 13, value: "chicken 65" },
+  //   { id: 14, value: "chiken varuval" },
+  // ]);
+
+  // const nestedObjects = Catergories.map((Catergories) => {
+  //   if (Catergories.value === "south indian") {
+  //     return (
+  //       <Dropdown.Item
+  //         children={Catergories.id}
+  //         key={Catergories.id}
+  //         className="item"
+  //       >
+  //         <p key={Catergories.id}>
+  //           {Catergories.value} <i className="fa fa-sort-desc icon"></i>
+  //         </p>
+  //         <Dropdown.Submenu position="right">
+  //           {dishes.map((dishes) => {
+  //             if (dishes.value === "chicken") {
+  //               return (
+  //                 <Dropdown.Item key={dishes.id} className="item">
+  //                   <p key={dishes.id}>{dishes.value}</p>
+  //                   <i className="fa fa-sort-desc icon"></i>
+  //                   <Dropdown.Submenu position="right">
+  //                     {variety.map((variety) => (
+  //                       <Dropdown.Item key={variety.key} className="item">
+  //                         <p key={variety.key}> {variety.value}</p>
+  //                       </Dropdown.Item>
+  //                     ))}
+  //                   </Dropdown.Submenu>
+  //                 </Dropdown.Item>
+  //               );
+  //             } else
+  //               return (
+  //                 <Dropdown.Item key={dishes.id} className="item">
+  //                   <p key={dishes.id}>{dishes.value}</p>
+  //                 </Dropdown.Item>
+  //               );
+  //           })}
+  //         </Dropdown.Submenu>
+  //       </Dropdown.Item>
+  //     );
+  //   } else
+  //     return (
+  //       <Dropdown.Item key={Catergories.id} className="item">
+  //         <p>{Catergories.value}</p>
+  //       </Dropdown.Item>
+  //     );
+  // });
   return (
     <React.Fragment>
       <nav>
@@ -79,12 +77,52 @@ const App = () => {
           ></Dropdown>
 
           <Dropdown
-            title="south indian"
+            title="categories"
             position="right"
             buttonClassName="button"
             wrapperClassName="button"
           >
-            {nestedObjects}
+            <Dropdown.Item className="item">
+              <p>Continental</p>
+            </Dropdown.Item>
+            <Dropdown.Item className="item">
+              <p>punjabi</p>
+            </Dropdown.Item>
+            <Dropdown.Item className="item">
+              <p>chettinad</p>
+            </Dropdown.Item>
+            <Dropdown.Item className="item">
+              <p>
+                south indian <i className="fa fa-sort-desc icon"></i>
+              </p>
+              <Dropdown.Submenu position="right">
+                <Dropdown.Item className="item">
+                  <p>Dosa</p>
+                </Dropdown.Item>
+                <Dropdown.Item className="item">
+                  <p>Uthappam</p>
+                </Dropdown.Item>
+                <Dropdown.Item className="item">
+                  <p>Parotta</p>
+                </Dropdown.Item>
+                <Dropdown.Item className="item">
+                  <p>
+                    Chicken <i className="fa fa-sort-desc icon"></i>
+                  </p>
+                  <Dropdown.Submenu position="right">
+                    <Dropdown.Item className="item">
+                      <p>Chicken Masala</p>
+                    </Dropdown.Item>
+                    <Dropdown.Item className="item">
+                      <p>Chicke gravy</p>
+                    </Dropdown.Item>
+                    <Dropdown.Item className="item">
+                      <p>Chicken 65</p>
+                    </Dropdown.Item>
+                  </Dropdown.Submenu>
+                </Dropdown.Item>
+              </Dropdown.Submenu>
+            </Dropdown.Item>
           </Dropdown>
 
           <Dropdown
@@ -101,7 +139,7 @@ const App = () => {
           ></Dropdown>
         </ul>
       </nav>
-      <h4>Click South indian </h4>
+      <h4>Click catergories </h4>
     </React.Fragment>
   );
 };
